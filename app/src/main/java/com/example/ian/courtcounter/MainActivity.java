41bd3a11e1f77dbb35bacccdbd2b6e5ac1bc8b3c
ian-ndiba <ndibaian94@gmail.com>
2018-06-12 19:42:19 +0300

    first complete trial

--- app/src/main/java/com/example/ian/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/ian/courtcounter/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.ian.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String message;
+    private TextView newText, showText;
+    private Button newButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void textView(){
+        newText = (TextView) findViewById(R.id.textView1);
+        newButton = (Button) findViewById(R.id.button1);
+        showText = (TextView) findViewById(R.id.text);
+
+        newButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               message = newText.getText().toString();
+               Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+               showText.setText(message);
+            }
+        });
+
+    }
+
 }
